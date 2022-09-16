ff64d535113a429cc775285c3851c1a3ee187303
LaurentBartez <post.thomas@familieknorr.de>
2022-09-16 20:31:14 +0200

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,164 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { start } from 'repl';
+import * as vscode from 'vscode';
+import Task from './task';
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "markdown-todo-timer" is now active!');
+
+	function getActiveTasks(tasks:Task[]): Task[]{
+		let activeTasks:Task[] = [];
+		tasks.forEach(element => {
+			if (element.isActive){
+				activeTasks.push(element);
+			}
+		});
+		return activeTasks;
+	}
+
+	// This function shall toggle a timer for a given task.
+	// if an active task is in editor it will be toggled whereever it is
+	// in case of no active task, the one in selected line will be toggled
+	let disposable = vscode.commands.registerCommand('markdown-todo-timer.toggleTimer', () => {
+		
+		//check if selected line is a task
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) {
+			return
+		}
+
+		const tasks:Task[] = getTasks();
+		const rangesToToggle:vscode.Range[] = [];
+		const activeTasks = getActiveTasks(tasks);
+		const currentLine = activeEditor.selection.active.line;
+		if (activeTasks.length == 0){
+		
+			//no task is active start task in current line, if any
+			tasks.forEach(element => {
+				if (element.atLine(currentLine))
+				{
+					element.insertTimeStamp();
+					console.log("Started timer for: " + element.getTitle)
+				}
+			});
+		}
+		else
+		{
+			//one active task is there
+			activeTasks[0].insertTimeStamp();
+			console.log("Stopped timer for: " + activeTasks[0].getTitle)
+
+		}
+	});
+
+	context.subscriptions.push(disposable);
+
+
+
+	let timeout: NodeJS.Timer | undefined = undefined;
+
+	// create a decorator type that we use to decorate small numbers
+	const activeTaskDecorationType = vscode.window.createTextEditorDecorationType({
+		borderWidth: '1px',
+		borderStyle: 'solid',
+		overviewRulerColor: 'blue',
+		overviewRulerLane: vscode.OverviewRulerLane.Right,
+		light: {
+			// this color will be used in light color themes
+			borderColor: 'darkblue'
+		},
+		dark: {
+			// this color will be used in dark color themes
+			borderColor: 'lightblue'
+		},
+		after: {
+            contentText: 'active',
+            color: 'rgba(153, 153, 153)',
+          },
+	});
+
+	let activeEditor = vscode.window.activeTextEditor;
+
+	function getTasks() : Task[]{
+		var tasks:Task[] = [];
+		if (!activeEditor) {
+			return tasks;
+		}
+		const regEx = /- \[x\]|- \[ \]/g;
+		const text = activeEditor.document.getText();
+
+		let match;
+		const taskDeco: vscode.DecorationOptions[] = [];
+
+		while ((match = regEx.exec(text))) {
+ 
+			const startPos = activeEditor.document.positionAt(match.index);
+			const endPos = activeEditor.document.lineAt(startPos).range.end;
+
+			tasks.push(new Task(activeEditor,new vscode.Range(startPos, endPos)));
+		}
+		return tasks;		
+	}
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+
+		const taskDeco: vscode.DecorationOptions[] = [];
+		var tasks:Task[] = getTasks();
+
+		tasks.forEach(element => {
+			if (element.isActive){
+				const decoration = { range: element.getRange, hoverMessage: element.getInfo};
+				taskDeco.push(decoration);		
+			}	
+			
+		});
+		activeEditor.setDecorations(activeTaskDecorationType, taskDeco);
+
+	}
+
+	function triggerUpdateDecorations(throttle = false) {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		if (throttle) {
+			timeout = setTimeout(updateDecorations, 500);
+		} else {
+			updateDecorations();
+		}
+	}
+
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations(true);
+			console.log(event.contentChanges[0].text);
+			console.log(event.contentChanges[1].text);
+
+		}
+	}, null, context.subscriptions);
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
---
+++ src/task.ts
@@ -0,0 +1,89 @@
+import * as vscode from 'vscode';
+import * as moment from 'moment';
+
+
+class Task {
+    _textEditor: vscode.TextEditor;
+    _title: any;
+    _toggles: any;
+    _line: any;
+    _range: vscode.Range;
+    
+    get getRange():vscode.Range {
+        return this._range;
+    }
+
+    get getInfo():string {
+        return this._title;
+    }
+
+    get isActive():boolean {
+        return this._toggles.length % 2 == 1 ? true : false;
+    }
+
+    get getLine():Number{
+        return this._range.start.line;
+    }
+
+    get getTitle():Number{
+        return this._title;
+    }
+
+    public atLine(lineToCheck: Number): boolean{
+        return (this.getLine == lineToCheck);
+    }
+    public insertTimeStamp(){
+		const activeEditor = vscode.window.activeTextEditor;
+		if (activeEditor) {
+            const currentDate = Date.now();
+            this._toggles.push(currentDate);
+			const formattedDate = (moment(currentDate)).format('YYYY-MM-DD HH:mm');
+			activeEditor.edit(editBuilder => {
+				editBuilder.insert(this.getRange.end, " [" + formattedDate + "]");
+			});
+
+		}
+	}
+    static timeStamps(line: string):Date[] {
+        var tagToCheck = "[";
+        var startTags = line.indexOf(tagToCheck);
+        var content:Date[] = [];
+        const dateScheme = "YYYY-MM-DD HH:mm";
+        if (startTags > -1) {
+            line = line.substring(startTags);
+            var endTask = line.indexOf("]");
+            if (endTask > -1){
+                var data = line.substring(1,endTask);
+                if (data.length == dateScheme.length) {
+                    content.push(new Date(data));
+                }
+                line = line.substring(endTask +1);
+                if (line.length > 0){
+                    content = content.concat(this.timeStamps(line));
+                }
+            }
+        }
+        return content;    
+    }
+
+    private makeTitle(line: string): string{
+        var formattedTitle:string = line.trimStart();
+        formattedTitle = formattedTitle.substring("- [ ] ".length);
+        const idxFirstTag = formattedTitle.indexOf("[");
+        if (idxFirstTag > 0){
+            formattedTitle = formattedTitle.substring(0,idxFirstTag);
+        }
+        formattedTitle = formattedTitle.trimEnd(); 
+        return (formattedTitle);
+    }
+
+    constructor(textEditor: vscode.TextEditor,range: vscode.Range) {
+        this._textEditor = textEditor || null;
+        this._range = range || null;
+        this._line = this._textEditor.document.getText(this._range)|| null;
+        this._toggles = Task.timeStamps(this._line.substring(3));
+        this._title = this.makeTitle(this._line) || null;
+    }
+}
+
+export default Task;
